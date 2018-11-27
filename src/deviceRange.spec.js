d6e0479631086ce87a8df4731ebba280a8cd1bc5
Michele Biondi <michelebiondi01@gmail.com>
2018-11-27 16:59:38 +0100

    range

--- src/deviceRange.spec.js
+++ src/deviceRange.spec.js
@@ -29,4 +29,46 @@ describe("Device range structure tests", () => {
 
         expect(getRanges()[newRange.tag.toString()].ranges.length).toBe(1);
     });
+
+    it("should store the latest range if it receives a duplicate from the same anchor on the same tag", () => {
+        const newRange = {
+            anchor: 0x1,
+            tag: 0x5,
+            range: 20.2,
+        };
+
+        const newnewRange = { ...newRange, range: 18 };
+
+        addRange(newRange);
+        addRange(newnewRange);
+
+        expect(getRanges()[newnewRange.tag.toString()].ranges).toMatchObject([{
+            anchor: 0x1,
+            range: 18,
+        }]);
+    });
+
+    it("should empty target tag after 3 ranges from 3 different anchors", () => {
+        const rangeA = {
+            anchor: 0x1,
+            tag: 0x5,
+            range: 20.2,
+        };
+
+        const rangeB = {
+            ...rangeA,
+            anchor: 0x02
+        };
+
+        const rangeC = {
+            ...rangeA,
+            anchor: 0x03
+        };
+
+        addRange(rangeA);
+        addRange(rangeB);
+        addRange(rangeC);
+
+        expect(getRanges()[rangeA.tag.toString()].ranges.length).toBe(0);
+    });
 });
--- src/devicesRange.js
+++ src/devicesRange.js
@@ -13,9 +13,20 @@ exports.addRange = (newRange) => {
         .filter((range) => range.anchor === newRange.anchor)
         .length === 0
     ) {
-            
-            devices[key].ranges.push({anchor: newRange.anchor, range: newRange.range});
-        }
+        devices[key].ranges.push({anchor: newRange.anchor, range: newRange.range});
+    } else {
+        devices[key].ranges = devices[key]
+                                .ranges
+                                .map((rangeStored) => 
+                                    rangeStored.anchor === newRange.anchor
+                                    ? {...rangeStored, range: newRange.range}
+                                    : rangeStored
+                                );
+    }
+
+    if(devices[key].ranges.length === 3) {
+        devices[key].ranges = [];
+    }
 };
 
 exports.getRanges = () => devices;
