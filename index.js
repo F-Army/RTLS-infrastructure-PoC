b3d68e0fc143c8fcccdd809e09c8a9a25ea83a3a
Michele Biondi <michelebiondi01@gmail.com>
2018-11-28 19:38:17 +0100

    add index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+
+const anchors = require("./src/anchors");
+const rangeRoute = require("./src/range");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use((req,res,next) => {
+  console.log(req.body);
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.use("/anchor", anchors.router);
+app.use("/range", rangeRoute);
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
