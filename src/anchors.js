fde8d9e47b6c1824b57c0b8992ac78405291ac2c
Michele Biondi <michelebiondi01@gmail.com>
2018-11-29 17:13:10 +0100

    Improved logic

--- src/anchors.js
+++ src/anchors.js
@@ -2,12 +2,20 @@ const Joi = require("joi");
 const express = require("express");
 const router = express.Router();
 
-const { anchorScheme, getAnchors, addAnchor } = require("./model/anchors");
+const { getAnchors, addAnchor } = require("./model/anchors");
 
 const euiAlreadyPresent = (eui) => {
     const arrayAnchors = Array.from(getAnchors().values());
     return arrayAnchors.filter((anchor) => anchor.eui === Number(eui)).length === 1;
 }
+
+const anchorScheme = Joi.object().keys({
+    eui: Joi.number().min(0).max(0xFFFFFFFFFFFFFFFF).required(),
+    short: Joi.number().min(0).max(0xFFFF).required(),
+    x: Joi.number().required(),
+    y: Joi.number().required(),
+    z: Joi.number().required(),
+});
 
 router.post("/", async (req, res) => {
     const validation = Joi.validate(req.body, anchorScheme);
--- src/model/anchors.js
+++ src/model/anchors.js
@@ -2,14 +2,6 @@ const Joi = require("joi");
 
 let anchors = new Map();
 
-exports.anchorScheme = Joi.object().keys({
-    eui: Joi.number().min(0).max(0xFFFFFFFFFFFFFFFF).required(),
-    short: Joi.number().min(0).max(0xFFFF).required(),
-    x: Joi.number().required(),
-    y: Joi.number().required(),
-    z: Joi.number().required(),
-});
-
 exports.getAnchors = () => anchors;
 
 exports.addAnchor = (anchor) => {
